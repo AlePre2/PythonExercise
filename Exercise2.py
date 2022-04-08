8cf9030be95c14f22b17a847e0cfa73c864f54e9
Ale3D <aleprecom@gmail.com>
2022-04-08 17:24:18 +0200

    Update Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -2,27 +2,27 @@ import math
 
 #Circle
 
-from termios import CR1, CR2
-
-
 class Circle:
-    def __init__(self, radius, diameter):
+    def __init__(self, radius):
 
         def get_area(self) -> float:
             return (math.pi * (self.radius ** 2))
 
+    def __Diameter__(self.radius):
+        return self.radius * 2
+
     def __add(self, radius, diameter):
         return self.radius + self.radius
+
     def __eq__(self, other):
         return self.radius == other.radius
+
     def __gt__(self, other):
         return self.radius > other.radius
+
     def __lt__(self, other):    
         return self.radius < other.radius
     
-
-
-
 #Array
 """
 SparseArray =  [1,2,0,0,0,0,3,0,0,4]
