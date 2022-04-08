588bc3713ecb63cd3f255ae6064d7a077b9cc4ac
Ale3D <aleprecom@gmail.com>
2022-04-08 17:22:10 +0200

    Create Exercise2.py

---
+++ Exercise2.py
@@ -0,0 +1,31 @@
+import math
+
+#Circle
+
+from termios import CR1, CR2
+
+
+class Circle:
+    def __init__(self, radius, diameter):
+
+        def get_area(self) -> float:
+            return (math.pi * (self.radius ** 2))
+
+    def __add(self, radius, diameter):
+        return self.radius + self.radius
+    def __eq__(self, other):
+        return self.radius == other.radius
+    def __gt__(self, other):
+        return self.radius > other.radius
+    def __lt__(self, other):    
+        return self.radius < other.radius
+    
+
+
+
+#Array
+"""
+SparseArray =  [1,2,0,0,0,0,3,0,0,4]
+print(len(SparseArray))
+print(SparseArray[5])
+"""
